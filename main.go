d8f5bedf786b14ca002c8d911a06d808c17bb239
Luis Fernando Raga <luis.raga@laika.com.co>
2021-02-24 20:57:43 -0500

    finish feature

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"keny-go/controllers"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	controllers.InitIndex()
+	router := gin.Default()
+	router.Static("/public", "public")
+	router.LoadHTMLGlob("views/*")
+
+	// Simple group: v1
+	v1 := router.Group("/api")
+	{
+		v1.GET("/init", controllers.InitIndex)
+
+	}
+	router.Run(":4000")
 }
