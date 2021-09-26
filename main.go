a3fdd509e7a44d3affe84872df1e3e6d2c73cde6
Luis Fernando Raga Renteria <whary11@gmail.com>
2021-09-26 06:24:46 -0500

    ....

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	fmt.Println(utils.GetEnv("APP_PORT", ":4001"))
 	// models.SaveTablesAndData()
 
-	port := utils.GetEnv("APP_PORT", "4001")
+	port := "4001"
 	router := gin.Default()
 	//// Rutas
 	routes.Api(router)
